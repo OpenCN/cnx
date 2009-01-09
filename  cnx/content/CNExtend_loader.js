60c03ed3e456050877e816c267b6c2410545b35f
mindstormskd <mindstormskd@fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87>
2009-01-09 17:29:35 +0000

    Cleanup, style change ;)
    git-svn-id: http://cnx.googlecode.com/svn/trunk@13 fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87

---  cnx/content/CNExtend_loader.js
+++  cnx/content/CNExtend_loader.js
@@ -1,7 +1,6 @@
-
 //We have this as a separate file for testing purposes.  We don't want this to be attached to the page
 //when we are testing CNExtend_main and others in JSUnit.
 CNExtend_scripts.loadCNXULScripts();
 
-window.addEventListener("load", function() { CNExtend_main.init(); }, false);
-window.addEventListener("unload", function() {CNExtend_main.cleanup();}, false);
+window.addEventListener("load", function(){ CNExtend_main.init(); }, false);
+window.addEventListener("unload", function(){ CNExtend_main.cleanup(); }, false);
