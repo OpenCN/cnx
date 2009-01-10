3f65f4e856b756bd4f62eff76b3997c45403e77e
mindstormskd <mindstormskd@fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87>
2009-01-10 22:56:08 +0000

    cleanup
    git-svn-id: http://cnx.googlecode.com/svn/trunk@22 fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87

--- content/CNExtend_enum.js
+++ content/CNExtend_enum.js
@@ -1,16 +1,15 @@
+// cleaned
 //Lists all the enumerations that we need
-
-
 var CNExtend_enum = new function() {
 	const _prefPrefix = "extensions.cnextend.";
 	
-	this.pageType = {Invalid:0, Other:11, StandardView:12, BattleView:13, BattleResult:14, ExtendedView:15, DebugFile:16};
-	this.logLevel = {Invalid:0, None:10, Normal:11, Debug:12};
-	this.errorType = {None:0, Unknown:11, Validation:12, CriticalFileMissing:13, CopyFiles:14, Transformation:15, ParseImprovementTable:16};
-	this.messageType = {Invalid: 0, CriticalError: 1, Warning : 2, VersionMessage : 3};
+	this.pageType = { Invalid: 0, Other: 11, StandardView: 12, BattleView: 13, BattleResult: 14, ExtendedView: 15, DebugFile: 16 };
+	this.logLevel = { Invalid: 0, None: 10, Normal: 11, Debug: 12};
+	this.errorType = { None: 0, Unknown: 11, Validation: 12, CriticalFileMissing: 13, CopyFiles: 14, Transformation: 15, ParseImprovementTable: 16 };
+	this.messageType = { Invalid: 0, CriticalError: 1, Warning: 2, VersionMessage: 3 };
 	this.website = "http://www.babelphish.net/cnextend/";
 	this.nationPath = "/nation_drill_display.asp";
-	this.improvementPath = "/improvements_purchase.asp"
+	this.improvementPath = "/improvements_purchase.asp";
 	this.wonderPath = "/national_wonders_purchase.asp";
 	this.helpPage = "cnextend-faq-and-error-guide";
 	this.backupDirectoryName = "backup";
@@ -22,5 +21,4 @@ var CNExtend_enum = new function() {
 	this.TIPS_ENABLED_PREF = _prefPrefix + "tips.areEnabled";
 	this.PLAYER_DATA_PREF = _prefPrefix + "playerData";
 	this.MESSAGES_PREF = _prefPrefix + "messages";
-	
 };
