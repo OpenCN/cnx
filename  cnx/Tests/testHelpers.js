c78b0cecf01e53873cf7e4b5a29b157e435b9933
babelphish.fark <babelphish.fark@fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87>
2009-05-18 00:43:59 +0000

    Finally got jsunit working in FF3, most tests were broken so we're starting over.  Added test for land parsing.
    git-svn-id: http://cnx.googlecode.com/svn/trunk@105 fe98914c-ca2c-11dd-a2c4-6dc3ad32ce87

---
+++  cnx/Tests/testHelpers.js
@@ -0,0 +1,30 @@
+
+CNExtend_util.error = function(exception, type, critical)
+{
+	alert(exception);
+}
+
+CNExtend_util.debugLog = function(message)
+{
+	
+	
+}
+
+CNExtend_util.PrefObserver = {};
+CNExtend_util.PrefObserver.setStringPreference = function(){};
+CNExtend_util.PrefObserver.getStringPreference = function(){};
+
+
+var resourcePage = '../Tests/data/discount_resources.htm';
+var myselfPage = '../Tests/data/myself_main.debug.cn.htm';
+var fullImprovementsPage = '../Tests/data/all_improvements_list.htm';
+var someoneElsePage = '../Tests/data/someone_else.htm';
+var someoneElseValidatorFile = '../content/Validation/otherDescription.xml';
+var transformation_file = '../Tests/data/layoutLoader.xml';
+var transformation_file_duplicates = '../Tests/data/layoutDuplicates.xml';
+var transformation_file_missing_attrib = '../Tests/data/layoutLoader_broken.xml';
+var selfValidatorFile_v20 = '../Tests/data/selfDescription_v20.xml';
+var selfValidatorFile_good = '../Tests/data/selfDescription_good.xml';
+var selfValidatorFile_wrong = '../Tests/data/selfDescription_wrong.xml';
+
+  
